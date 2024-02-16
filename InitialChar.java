d686392875be1816ad9538c3432d13541adc5ede
Jaiganesh R <113334074+Jaiganesh-Ram@users.noreply.github.com>
2024-02-16 19:52:34 +0530

    Add files via upload

---
+++ InitialChar.java
@@ -0,0 +1,7 @@
+public class InitialChar{
+public static void main(String[]args){
+char initial='P';
+System.out.println("initial letter of my first name:" + initial);
+}
+}
+
---
+++ Temperaturevalue.java
@@ -0,0 +1,6 @@
+public class TemperatureValue{
+public static void main(String[]args){
+double Temperature=98.6;
+System.out.println(Temperature);
+}
+}
