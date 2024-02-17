b161c498c3e0002e946e3d81baa6586c17d2699c
Jaiganesh R <113334074+Jaiganesh-Ram@users.noreply.github.com>
2024-02-17 22:33:22 +0530

    update the Opretors java tasks6,7,8and9

---
+++ Average.java
@@ -0,0 +1,8 @@
+public class Average{
+public static void main(String[] args){
+int num1=10;
+int num2=5;
+int result=num1+num2/2;
+System.out.println(result);
+}
+}
---
+++ EvenOrAdd.java
@@ -0,0 +1,11 @@
+public class EvenOrAdd{
+public static void main(String[] args){
+int num1=11;
+if (num1%2==0){
+System.out.println("is Even");
+}
+else{
+System.out.println("Add");
+}
+}
+}
